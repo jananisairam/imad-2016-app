47c5daaf81485120c702d59849d953666d829941
jananisairam <januakshitha97@gmail.com>
2016-10-09 10:22:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,14 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //move the image
 var img = document.getElementById('madi');
-img.onclick = function() {
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 
-    img.style.marginLeft='100px'
+img.onclick = function() {
+  var interval = setInterval(moveRight,100);  
+    img.style.marginLeft='100px';
 };
