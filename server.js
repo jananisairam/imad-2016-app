7128785a145bb3097422e096067802a261b5595e
jananisairam <januakshitha97@gmail.com>
2016-10-09 13:15:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,15 +75,16 @@ var ArticleOne = {
             <div> ${content} </div>
             </div>
             </body>
-            </html>`
-;
+            </html>
+            `;
+            return htmlTemplate;
 }
             
 app.get('/', function (req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 app.get('/article-one', function (req,res){
- res.sendFile(path.join(__dirname,'ui','article-one.html'));   
+ res.send(createTemplate(articleOne));  
 });
 app.get('/article-two', function (req,res){
 res.sendFile(path.join(__dirname,'ui','article-two.html'));
