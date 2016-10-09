8d5d94d2d92d3472b9f2543790951b16e184b051
jananisairam <januakshitha97@gmail.com>
2016-10-09 13:34:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,12 +83,11 @@ var articles = {
 app.get('/', function (req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
-app.get('/:articleName ', function (req,res){
+app.get('/:articleName', function (req, res) {
 //articleName == article-one
 //articles[articleName] == {} content objecct for article one
 var articleName = req.params.articleName;
 res.send(createTemplate(articles[articleName]));  
-
 });
 
 app.get('/ui/style.css', function (req,res){
