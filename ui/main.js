1cad597fa1914335e5e3becde976647707e502ff
jananisairam <januakshitha97@gmail.com>
2016-10-09 18:35:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,11 +29,11 @@ button.onclick = function() {
     //make a request to server and send the name
     
     //capture a list of name and render it in a list.
-    var names = ['name1','name2','name3'];
+    var names = ['name1','name2','name3','name4'];
     var list ='';
     for(var i=0;i<names.length;i++) {
         list += '<li>' + names[i] + '</li>';
     }
 };
-var ul =document.getElementById('namelist');
-ul.innerHTML = namelist;
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
