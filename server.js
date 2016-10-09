a0e83aeb2754616cdcd61be9aa3a8256f2075b95
jananisairam <januakshitha97@gmail.com>
2016-10-09 18:25:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,6 @@ app.get('/ui/main.js', function (req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
-
 app.get('/ui/madi.png', function (req,res){
     res.sendFile(path.join(__dirname,'ui','madi.png'));
 });
