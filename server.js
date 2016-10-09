e7b522fff506cb22e2a1f18545c55666c2b4b355
jananisairam <januakshitha97@gmail.com>
2016-10-09 13:45:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,8 +80,13 @@ var articles = {
             return htmlTemplate;
 }
             
-app.get('/', function (req,res){
+app.get('/', function (req, res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
+});
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 app.get('/:articleName', function (req, res) {
 //articleName == article-one
@@ -93,6 +98,11 @@ res.send(createTemplate(articles[articleName]));
 app.get('/ui/style.css', function (req,res){
     res.sendFile(path.join(__dirname,'ui','style.css'));
 });
+
+app.get('/ui/main.js', function (req,res){
+    res.sendFile(path.join(__dirname,'ui','main.js'));
+});
+
 
 app.get('/ui/madi.png', function (req,res){
     res.sendFile(path.join(__dirname,'ui','madi.png'));
